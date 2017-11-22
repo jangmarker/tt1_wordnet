121d151ce69833511bc6e72486a5ebccefa20989
Jan Marker <jan@jangmarker.de>
2017-11-22 14:32:37 +0100

    Add command line options for first task

--- main.cpp
+++ main.cpp
@@ -1,27 +1,39 @@
 #include "FileAccess.h"
 #include "Database.h"
 
+#include "3rdparty/CLI11.hpp"
+
 #include <iostream>
 
 static const std::string databaseFolder("/home/privat/Projects/WordNet-3.0/dict/");
 
-int main() {
-    std::string pos("a");
-    std::string indexWord("sweet");
-    bool full = true;
+int main(int argc, char** argv) {
+    CLI::App app{"Text Technologie 1 - Aufgabe 2 - Lösung Jan Marker"};
 
-    FileAccess fileAccess(databaseFolder);
-    Database database(fileAccess);
+    std::string partOfSpeech;
+    std::string indexWord;
 
-    const auto synsets = database.synsetsByIndexWord(pos, indexWord);
+    auto* query = app.add_subcommand("query");
 
+    query->add_set("--pos", partOfSpeech, {"n", "a", "v", "r"}, "Part of Speech")->required();
+    query->add_option("--iw", indexWord, "Index word")->required();
+    auto full = query->add_flag("--full", "Show index words for all synsets");
 
-    if (synsets.empty()) {
-        std::cout << "Could not find" << indexWord << std::endl;
-    } else {
-        for (const auto &synset : synsets) {
-            // TODO format offset and type
-            std::cout << synset.type << " " << synset.offset << synset.gloss << std::endl;
+    CLI11_PARSE(app, argc, argv);
+
+    if (*query) {
+        FileAccess fileAccess(databaseFolder);
+        Database database(fileAccess);
+
+        const auto synsets = database.synsetsByIndexWord(partOfSpeech, indexWord);
+
+        if (synsets.empty()) {
+            std::cout << "Could not find " << indexWord << std::endl;
+        } else {
+            for (const auto &synset : synsets) {
+                // TODO format offset and type
+                std::cout << synset.type << " " << synset.offset << synset.gloss << std::endl;
+            }
         }
     }
 
